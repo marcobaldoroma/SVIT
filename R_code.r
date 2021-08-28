dea3fbec7a22a4d7480def9b140e850185d68382
Marco Baldo <marco.baldo2@studio.unibo.it>
2021-08-28 23:30:30 +0200

    Update R_code.r

--- R_code.r
+++ R_code.r
@@ -1,4 +1,8 @@
-## Multiple Time Series Analysis on DMP, FAPAR, NDVI V2 1Km spatial scale from VITO catalogues 1999-2020 of the WG region
+# Satellite Vegetation Indices Trend (SVIT) is an Algorithm created by the Ph.D. student Baldo Marco to analyze the forest changes in terms of cover and productivity. 
+# Together with PhD Piero Zannini, PhD student Kurian Ayushi and the Full Professor of the University of Bologna Duccio Rocchini, 
+# We create an R script to analize the Dry Matter Productivity, Fraction of Absorbed Photosintetically Active Radiation and Normalized Different Vegetation Index trends of a selected area, in this case over the Western Ghats world Biodiversity Hotspot.
+# We realized this analysis under the theory of the multispectral bands analysis and multiple time series analysis.
+# Multiple Time Series Analysis on DMP, FAPAR, NDVI V2 1Km spatial scale from VITO catalogues 1999-2020 of the WG region
 
 library(ncdf4)
 library(raster)
